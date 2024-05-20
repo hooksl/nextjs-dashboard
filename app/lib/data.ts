96b0c55354e63c41ec2f7109442b5774decfb8d1
lightning <lightning004@outlook.com>
2024-05-21 06:12:07 +0800

    并行获取数据
    await Promise.all

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,6 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+     // 使用并行获取数据 await Promise.all
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
