83c2b597ba698781424f6dd46c840dabed825553
lightning <lightning004@outlook.com>
2024-05-21 17:41:47 +0800

    继续添加部分组件

--- app/dashboard/(overviem)/page.tsx
+++ app/dashboard/(overviem)/page.tsx
@@ -3,14 +3,12 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {
-    fetchLatestInvoices,
     fetchCardData,
 } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfInvoices,
         numberOfCustomers,
@@ -33,7 +31,9 @@ export default async function Page() {
                     {/* <RevenueChart revenue={revenue} /> */}
                     {<RevenueChart />}
                 </Suspense>
-                {<LatestInvoices latestInvoices={latestInvoices} />}
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    {<LatestInvoices />}
+                </Suspense>
             </div>
         </main>
     );
