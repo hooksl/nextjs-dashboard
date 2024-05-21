107a5a67ad63cd03b27a10014d3a706c2f3165a4
lightning <lightning004@outlook.com>
2024-05-21 14:46:04 +0800

    预加载页面

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
-import sidenav from "@/app/ui/dashboard/sidenav";
 
 export default function Layout({children}:{children:React.ReactNode}) {
     return(
